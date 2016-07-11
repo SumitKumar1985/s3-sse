b3c5d5d435769e34b11984d1105b97ce1bbdd06b
Krishnan Mani <km@krishnanm.com>
2016-07-11 19:16:18 +0530

    Only create the bucket in setUp if it does not already exist, more Python idiom added

--- app/test_s3.py
+++ app/test_s3.py
@@ -1,26 +1,35 @@
 import unittest
 import boto3
+import botocore
+
+BUCKET = 'km-sse-random137'
 
 class TestS3(unittest.TestCase):
 
     def setUp(self):
         client = boto3.client('s3')
-        response = client.create_bucket(
-            Bucket='km-sse-random137'
-        )
+        try:
+            client.head_bucket(
+                Bucket = BUCKET
+            )
+        except botocore.exceptions.ClientError as e:
+            response = client.create_bucket(
+                Bucket = BUCKET
+            )
 
-    def test_save_file(self):
+
+    def test_put_object_with_sse(self):
         client = boto3.client('s3')
         response = client.get_bucket_location(
-            Bucket='km-sse-random137'
+            Bucket = BUCKET
         )
         self.assertEqual(None, response['LocationConstraint'])
 
 
     def tearDown(self):
         client = boto3.client('s3')
-        response = client.delete_bucket(
-            Bucket='km-sse-random137'
+        client.delete_bucket(
+            Bucket = BUCKET
         )
 
 
